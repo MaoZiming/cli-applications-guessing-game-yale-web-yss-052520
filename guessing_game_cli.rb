78f9166c8b9817408110a1c04a0ed28e6e9d6c17
Ziming Mao <ziming.mao@yale.edu>
2020-05-09 17:21:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,7 +2,7 @@
 def run_guessing_game
   num = rand(6)+1
   u_num = gets
-  if num == u_num
+  if num == u_num.to_i
     puts "You guessed the correct number!"
   elsif u_num == "exit"
     puts "Goodbye!"
