3ab392e67b7713f0608d785b81eb3c40711ec33a
Ziming Mao <ziming.mao@yale.edu>
2020-05-09 17:19:41 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,11 +1,10 @@
 # Code your solution here!
 def run_guessing_game
   num = rand(1..6)
-  puts "guess!"
   u_num = gets
   if num == u_num
     puts "You guessed the correct number!"
-  elsif u_num == "Goodbye!"
+  elsif u_num == "exit"
     puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{num}."
